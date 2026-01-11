d678e250de6f4e82ff6179fb925dacc9ab974abe
Sheikh-Rasel2001 <mhrasel191132@gmail.com>
2026-01-11 16:23:36 +0600

    more problem

--- src/Pages/Project/Projects.jsx
+++ src/Pages/Project/Projects.jsx
@@ -29,7 +29,7 @@ const Projects = () => {
                     {projects.map((project, index) => (
                         <motion.div
                             key={project.id}
-                            initial={{ opacity: 0, y: 40 }}
+                            initial={{ opacity: 0, y: 20 }}
                             whileInView={{ opacity: 1, y: 0 }}
                             transition={{ duration: 0.8, delay: index * 0.1 }}
                             viewport={{ once: true }}
