4cb05aff88b77d8a22ccee113233e68fd193af11
Sheikh-Rasel2001 <mhrasel191132@gmail.com>
2026-01-11 16:21:01 +0600

    fix a problem

--- src/Pages/Project/Projects.jsx
+++ src/Pages/Project/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
 
     return (
         <motion.div
-            initial={{ opacity: 0, y: 30 }}
+            initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, ease: "easeOut" }}
             viewport={{ once: false, amount: 0.2 }}
